a88f6750493d98c67dfb10f409fb5ceaaa289bc0
Juan Sebastian Tellez Melo <js.tellez10@uniandes.edu.co>
2025-09-24 15:57:29 -0500

    Update Master_1.R

--- scripts/Master_1.R
+++ scripts/Master_1.R
@@ -1,30 +1,33 @@
 ##########################################################
 # Taller 1 - Económia Urbana
-# ejercicio 1
-# author: 
+# Ejercicio 1
+# author: Eimmy Nicoll Tovar Escobar y Juan Sebastian Tellez Melo
 ##########################################################
 
 # Clean the workspace -----------------------------------------------------
 
 rm(list=ls())
 
-#Definir directorios -----------------------------------------------------
+# Definir directorios -----------------------------------------------------
 
 usuario <- tolower(Sys.info()[["user"]])  # o: tolower(Sys.getenv("USERNAME", Sys.getenv("USER")))
 
-# Mapeo de rutas
 rutas <- list(
   sebas = "C:/Users/sebas/OneDrive - RADDAR/Documentos/Documents/Sebastian Tellez/MAESTRIA/ECONOMIA URBANA/TALLER/TALLER 1/Taller_1_EU/",
   mora  = "C:/Users/mora/Path/To/TALLER/TALLER 1/"
 )
 
-# Validación y asignación
-if (!usuario %in% names(rutas)) {
-  stop(sprintf("Usuario no reconocido (%s). Usuarios permitidos: %s",
-               usuario, paste(names(rutas), collapse = ", ")))
-}
+root <- rutas[[usuario]]
 
-directorio <- rutas[[usuario]]
+stores <- file.path(root, "stores")
+scripts <- file.path(root, "scripts")
+views <- file.path(root, "views")
+
+# Load Packages -----------------------------------------------------------
+
+# Cargar datos -----------------------------------------------------------
+
+
 
 
 
